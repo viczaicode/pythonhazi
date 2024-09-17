58722b1643a9d84b1aff5a7dcda4706adedca5b7
viczaicode <viczaimilan2003@gmail.com>
2024-09-17 22:54:30 +0200

    kesz

---
+++ nyugtaaa.py
@@ -0,0 +1,37 @@
+ar1 = int(input("Kérem adjon meg egy összeget: "))
+ar2 = int(input("Kérem adjon meg egy másik összeget: "))
+ar3 = int(input("Kérem adjon meg egy harmadik összeget: "))
+vegeredmeny = ar1 + ar2 +ar3
+
+
+print("************************")
+
+print("*       NYUGTA         *")
+
+print("************************")
+
+print(f"tétel1        {ar1} Ft")
+
+print(f"tétel2          {ar2} Ft")
+
+print(f"tétel3         {ar3} Ft")
+
+print("========================")
+
+print(f"Összesen:     {vegeredmeny} Ft")
+
+print("Szervízdíj:    151.17 Ft")
+
+print("========================")
+
+print(f"Fizetendő:    {vegeredmeny} Ft")
+
+print("---------      ---------")
+
+print(" Dátum            Név   ")
+
+print("************************")
+
+print("*         CÉG          *")
+
+print("************************")
